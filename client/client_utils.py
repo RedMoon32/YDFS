34a0b343c692cf7ebe68c64e04839718a63f66e3
Fenrir7Asteron <b.fedotov@innopolis.ru>
2019-11-18 21:25:58 +0300

    Add client side put of a file. Masternode returns a list of datanodes with /file POST.

---
+++ client/client_utils.py
@@ -0,0 +1,53 @@
+import requests
+import os
+from flask import Response
+
+MAX_REQUEST_COUNT = 3
+
+
+def check_response(resp):
+    if resp.status_code == 200:
+        print(resp.content.decode())
+        return 0
+    else:
+        print("Error:", resp.content.decode())
+        return 1
+
+
+def check_args(command, args, missing_operands):
+    if len(args) < 2:
+        print(f'{command}: missing {missing_operands[0]} operand')
+        return 1
+    for i in range(1, len(missing_operands)):
+        if len(args) < i + 2:
+            print(f"{command}: missing {missing_operands[i]} operand after '{args[i]}'")
+            return 1
+    # Check if extra operands are present
+    if len(args) - 1 > len(missing_operands):
+        print(f'{command}: extra operands are present, expected [{len(missing_operands)}] - got [{len(args) - 1}]')
+        return 1
+    return 0
+
+
+def request_datanodes(datanodes, command, method, data=None):
+    resp = None
+    for datanode in datanodes:
+        node_address = f"{datanode['ip']}:{datanode['port']}"
+        for try_counter in range(MAX_REQUEST_COUNT):
+            if method == "GET":
+                resp = requests.get(os.path.join(node_address, command), data=data)
+            elif method == "POST":
+                resp = requests.post(os.path.join(node_address, command), data=data)
+            elif method == "DELETE":
+                resp = requests.delete(os.path.join(node_address, command), data=data)
+            if resp.status_code == 200:
+                return Response(status=200)
+    return resp
+
+
+def read_file(path):
+    try:
+        return open(path, 'rb').read()
+    except OSError as e:
+        print(e)
+        return None
